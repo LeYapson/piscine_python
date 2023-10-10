c89955aa47684011637c126c576f21e5dc64d909
LeYapson <theau.yapi@ynov.com>
2023-10-10 16:27:36 +0200

    eivcifdijv

--- shopping.py
+++ shopping.py
@@ -8,20 +8,20 @@ def remember_the_milk(shopping_list):
     return shopping_list
 
 def clean_list(shopping_list):
+    if not shopping_list:
+        return []
 
-  # Add milk at the end of the list if missing.
-  if "milk" not in shopping_list:
-    shopping_list.append("milk")
+    # Add milk at the end of the list if missing.
+    if "milk" not in shopping_list:
+        shopping_list.append("milk")
 
-  cleaned_list = []
-  for index, item in enumerate(shopping_list):
-    # Remove all spaces before and after.
-    item = item.strip()
+    cleaned_list = []
+    for index, item in enumerate(shopping_list):
+        # Remove all spaces before and after.
+        item = item.strip()
 
-    # Capitalize the first letter.
-    item = item.capitalize()
-
-    # Add the index number and separator.
-    cleaned_list.append(f"{index + 1}/ {item}")
-
-  return cleaned_list
+        # Capitalize the first letter.
+        item = item.capitalize()
+        # Add the index number and separator.
+        cleaned_list.append(f"{index + 1}/ {item}")
+    return cleaned_list
