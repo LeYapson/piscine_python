19034a90da3e7157162959b74ea4abe2ca7076aa
LeYapson <theau.yapi@ynov.com>
2023-10-10 16:24:39 +0200

    eivcifdijv

--- shopping.py
+++ shopping.py
@@ -8,18 +8,10 @@ def remember_the_milk(shopping_list):
     return shopping_list
 
 def clean_list(shopping_list):
-  """Cleans a list of strings by removing all spaces before and after, capitalizing the first letter, and adding an index number and separator before each item.
-
-  Args:
-    shopping_list: A list of strings.
-
-  Returns:
-    A list of strings with the cleaned items.
-  """
 
   # Add milk at the end of the list if missing.
-  if "Milk" not in shopping_list:
-    shopping_list.append("Milk")
+  if "milk" not in shopping_list:
+    shopping_list.append("milk")
 
   cleaned_list = []
   for index, item in enumerate(shopping_list):
