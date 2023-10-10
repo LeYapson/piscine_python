8d1fdd3260c69fa7dd72eaf551788a724424294b
LeYapson <theau.yapi@ynov.com>
2023-10-10 17:48:08 +0200

    j'avais mis le main dans le fichier zebi

--- string_processing.py
+++ string_processing.py
@@ -1,17 +1,22 @@
-import string
-
 import re
 
 
 def tokenize(sentence):
-    # Remove punctuation and special characters except for apostrophes
-    sentence = re.sub(r'[^a-zA-Z0-9\s\']', '', sentence)
+    # Supprimer la ponctuation et les caractères spéciaux tout en préservant espaces, apostrophes et tirets
+    sentence = re.sub(r'[^\w\s\'-]', '', sentence)
 
-    # Split the sentence into words
-    words = re.split(r'\s+', sentence)
+    # Fractionner la phrase en mots tout en préservant les mots contenant des apostrophes et des tirets
+    words = re.findall(r'\b\w+(?:[-\']\w+)*\b', sentence)
 
-    # Convert words to lowercase
-    words = [word.lower() for word in words if word]  # Filter out empty words
+    # Séparer les mots contenant des apostrophes ou des tirets en mots individuels
+    final_words = []
+    for word in words:
+        if "'" in word or "-" in word:
+            final_words.extend(re.split(r"['-]", word))
+        else:
+            final_words.append(word)
 
-    return words
+    # Convertir les mots en minuscules
+    final_words = [word.lower() for word in final_words if word]  # Filtrer les mots vides
 
+    return final_words
