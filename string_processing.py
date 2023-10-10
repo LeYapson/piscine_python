7d97a5bf6f9af7332ac614aedb11363535d01c3c
LeYapson <theau.yapi@ynov.com>
2023-10-10 17:40:37 +0200

    j'avais mis le main dans le fichier zebi

--- string_processing.py
+++ string_processing.py
@@ -1,15 +1,17 @@
 import string
+
 import re
 
 
 def tokenize(sentence):
-    # Remove punctuation and special characters
-    sentence = re.sub(r'[^a-zA-Z0-9\s]', '', sentence)
+    # Remove punctuation and special characters except for apostrophes
+    sentence = re.sub(r'[^a-zA-Z0-9\s\']', '', sentence)
 
     # Split the sentence into words
-    words = sentence.split()
+    words = re.split(r'\s+', sentence)
 
     # Convert words to lowercase
-    words = [word.lower() for word in words]
+    words = [word.lower() for word in words if word]  # Filter out empty words
 
     return words
+
