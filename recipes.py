abc08651319ec9a30e43574da913d42aed05afdf
LeYapson <theau.yapi@ynov.com>
2023-10-10 17:58:12 +0200

    on speedrun ouuuuuuu

---
+++ recipes.py
@@ -0,0 +1,19 @@
+def create_recipe(name, persons, ingredients):
+    # Check if title length is within the limit
+    if len(name) > 150:
+        raise ValueError("Title is too long")
+
+    # Check if persons is null or greater than 50
+    if persons is None or persons > 50:
+        raise ValueError("Invalid persons number")
+
+    # Check if ingredients list is empty
+    if not ingredients:
+        raise ValueError("This recipe has no ingredients")
+
+    recipe = {
+        'title': name,
+        'persons': persons,
+        'ingredients': ingredients,
+    }
+    return recipe
