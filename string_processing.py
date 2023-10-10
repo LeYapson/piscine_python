7a765c5deb53770e63b5ad625460688645a7fcdf
LeYapson <theau.yapi@ynov.com>
2023-10-10 17:51:17 +0200

    j'avais mis le main dans le fichier zebi

--- string_processing.py
+++ string_processing.py
@@ -5,21 +5,13 @@ def tokenize(sentence):
     # Remplacer les apostrophes et tirets par des espaces pour pouvoir les séparer
     sentence = re.sub(r"['-]", ' ', sentence)
 
-    # Supprimer la ponctuation et les caractères spéciaux
-    sentence = re.sub(r'[^a-zA-Z0-9\s]', '', sentence)
+    # Utiliser une expression régulière spécifique pour les mots comme "c'est-à-dire"
+    words = re.findall(r'\b\w+(?:-\w+)*\b', sentence)
 
-    # Séparer la phrase en mots
-    words = sentence.split()
+    # Supprimer la ponctuation et les caractères spéciaux
+    words = [re.sub(r'[^a-zA-Z0-9]', '', word) for word in words]
 
     # Convertir les mots en minuscules
-    words = [word.lower() for word in words]
+    words = [word.lower() for word in words if word]  # Filtrer les mots vides
 
-    # Séparer les mots contenant des apostrophes ou des tirets en mots individuels
-    final_words = []
-    for word in words:
-        if "'" in word or "-" in word:
-            final_words.extend(re.split(r"['-]", word))
-        else:
-            final_words.append(word)
-
-    return final_words
+    return words
