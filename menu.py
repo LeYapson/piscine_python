2eddd6851a8d5d95384e391a028f963b6daf1c9b
LeYapson <theau.yapi@ynov.com>
2023-10-11 16:22:51 +0200

    oefdifjif

--- menu.py
+++ menu.py
@@ -9,3 +9,10 @@ def build_menu(recipes, start_date):
         current_date += dt.timedelta(days=1)
 
     return menu
+
+
+def save_menu(meals):
+    with open('menu.txt', 'w') as file:
+        for date, recipe in meals:
+            formatted_date = date.strftime("%A %d %B %Y")
+            file.write(f"{formatted_date}: {recipe}\n")
