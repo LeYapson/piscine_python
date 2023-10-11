b5d169f0217b43391815cfff1e98410577ca4630
LeYapson <theau.yapi@ynov.com>
2023-10-11 17:18:56 +0200

    oefdifjif

--- recipes.py
+++ recipes.py
@@ -21,16 +21,17 @@ def create_recipe(name, persons, ingredients):
 
 def create_recipe_v2(title, persons, *ingredients, **tags):
     # Check title length
-    if len(title) > 30:
+    # Check if title length is within the limit
+    if len(title) > 150:
         raise ValueError("Title is too long")
 
-    # Check persons
-    if persons <= 0 or persons > 20:
+    # Check if persons is null or greater than 50
+    if persons is None or persons > 50 or persons == 0:
         raise ValueError("Invalid persons number")
 
-    # Check ingredients
+    # Check if ingredients list is empty
     if not ingredients:
-        raise ValueError("Ingredient list cannot be empty")
+        raise ValueError("This recipe has no ingredients")
 
     # Return the recipe as a dictionary
     return {
