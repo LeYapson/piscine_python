5ed41775588ab1af3e5db27aee39d543f7278093
LeYapson <theau.yapi@ynov.com>
2023-10-11 22:12:02 +0200

    SA ME CASSE LES *********************** SE ********************** DE CLI.PY D'EMPAFEEEEEEEE, JE VEUX ME METTRE UNE BALLE C'EST TROP CHIANT ZEBIIIIIIIIIIIIIIIIIIIIIIIIII3

--- filter_recipes.py
+++ filter_recipes.py
@@ -1,2 +1,2 @@
-def filter_recipes(recipes, max_persons):
-    return [recipe['title'] for recipe in recipes if recipe['persons'] <= max_persons]
+def filter_recipes(recipes: list[dict], max_persons: int) -> list[str]:
+    return [recipe['title'] for recipe in recipes if recipe['persons']<max_persons]
