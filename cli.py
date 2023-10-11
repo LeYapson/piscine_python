cea862456f8c9a592642ef4560e0e9c539102ba0
LeYapson <theau.yapi@ynov.com>
2023-10-11 18:03:27 +0200

    oefdifjif

--- cli.py
+++ cli.py
@@ -1,57 +1,31 @@
-import datetime
-import json
-import sys
-
+import argparse
 from datetime_utils import format_date
-from filter_recipes import filter_recipes
 from menu import build_menu
-from recipes import create_recipe
+from filter_recipes import filter_recipes
 from sort_list import sort_recipes
+from read_recipes import get_recipes
 
-def parse_arguments():
-  """Parses the command line arguments.
+def main():
+    # Define and parse command-line arguments
+    parser = argparse.ArgumentParser(description="Generate a menu based on recipes data.")
+    parser.add_argument("-s", "--start", type=str, help="Start date in 'dd/mm/yyyy' format", required=True)
+    parser.add_argument("-p", "--max-persons", type=int, default=4, help="Maximum number of persons (default: 4)")
+    args = parser.parse_args()
 
-  Returns:
-    A tuple containing the start date and the maximum number of persons.
-  """
+    # Read recipes data from recipes_data.json
+    recipes = get_recipes("recipes_data.json")
 
-  start_date = None
-  max_persons = 4
+    # Sort recipes by title
+    recipes = sort_recipes(recipes, 'title')
 
-  for arg in sys.argv:
-    if arg in ["--start", "-s"]:
-      start_date = datetime.datetime.strptime(sys.argv[sys.argv.index(arg) + 1], "%Y-%m-%d")
-    elif arg in ["--max-persons", "-p"]:
-      max_persons = int(sys.argv[sys.argv.index(arg) + 1])
+    # Get the current week's dates
+    week_dates = format_date(args.start)
 
-  return start_date, max_persons
+    # Filter recipes based on the maximum number of persons
+    filtered_recipes = filter_recipes(recipes, args.max_persons)
 
-def generate_menu_from_recipes(recipes, start_date, max_persons):
-  """Generates a menu from a list of recipes.
+    # Create the menu and write it to menu.txt
+    build_menu(week_dates, filtered_recipes)
 
-  Args:
-    recipes: A list of recipes.
-    start_date: The start date.
-    max_persons: The maximum number of persons.
-
-  Returns:
-    A string containing the menu.
-  """
-
-  filtered_recipes = filter_recipes(recipes, max_persons)
-  sorted_recipes = sort_recipes(filtered_recipes, key=lambda recipe: recipe["title"])
-
-  menu = build_menu(start_date, sorted_recipes)
-
-  return menu
-
-def write_menu_to_file(menu, filename):
-  """Writes a menu to a file.
-
-  Args:
-    menu: A string containing the menu.
-    filename: The filename.
-  """
-
-  with open(filename, "w") as f:
-    f.write(menu)
+if __name__ == "__main__":
+    main()
