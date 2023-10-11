2c6c5ba76e312fb132d246cef8ceec96522970ae
LeYapson <theau.yapi@ynov.com>
2023-10-11 16:55:14 +0200

    oefdifjif

--- recipes.py
+++ recipes.py
@@ -18,25 +18,28 @@ def create_recipe(name, persons, ingredients):
     }
     return recipe
 
-def create_recipe_v2(title, persons, *ingredients, **tags):
-    try:
-        if len(title) > 50:
-            raise ValueError("Title is too long")
-        if persons <= 0:
-            raise ValueError("Invalid persons number")
-        if persons > 20:
-            raise ValueError("Too many persons")
-        if not ingredients:
-            raise ValueError("Ingredient list cannot be empty")
 
-        return {
-            'title': title,
-            'persons': persons,
-            'ingredients': ingredients,
-            'tags': tags
-        }
-    except ValueError as e:
-        return str(e)
+def create_recipe_v2(title, persons, *ingredients, **tags):
+    # Check title length
+    if len(title) > 50:
+        raise ValueError("Title is too long")
+
+    # Check persons
+    if persons <= 0 or persons > 20:
+        raise ValueError("Invalid persons number")
+
+    # Check ingredients
+    if not ingredients:
+        raise ValueError("Ingredient list cannot be empty")
+
+    # Return the recipe as a dictionary
+    return {
+        'title': title,
+        'persons': persons,
+        'ingredients': ingredients,
+        'tags': tags
+    }
+
 
 
 
