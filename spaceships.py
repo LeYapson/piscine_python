1f72f6c5899f4124d4a89ecb6f4018ef89365919
LeYapson <theau.yapi@ynov.com>
2023-10-12 16:43:54 +0200

    walla on fini

--- spaceships.py
+++ spaceships.py
@@ -2,6 +2,34 @@
 
 from base_spaceships import Fighter, Battleship
 
+class Interceptor(Fighter):
+    def __init__(self):
+        super().__init__(attack=180, defense=1000)
+
+
+
+class Bomber(Fighter):
+    def __init__(self):
+        super().__init__(attack=150, defense=2000)
+
+
+
+class Cruiser(Battleship):
+    def __init__(self):
+        super().__init__(attack=800, defense=3000)
+
+class Frigate(Battleship):
+    def __init__(self):
+        super().__init__(attack=500, defense=2500)
+
+
+
+class Destroyer(Battleship):
+    def __init__(self):
+        super().__init__(attack=650, defense=5000)
+
+
+
 class BattleshipKiller:
     def fire_on(self, target):
         if isinstance(target, Battleship):
@@ -15,22 +43,3 @@ class FighterKiller:
             target.take_damages(2 * self.attack)
         else:
             target.take_damages(self.attack)
-
-class Interceptor(FighterKiller, Fighter):
-    def __init__(self):
-        super().__init__(attack=180, defense=1000)
-
-class Bomber(BattleshipKiller, Fighter):
-    def __init__(self):
-        super().__init__(attack=150, defense=2000)
-
-class Destroyer(FighterKiller, Battleship):
-    def __init__(self):
-        super().__init__(attack=650, defense=5000)
-class Cruiser(Battleship):
-    def __init__(self):
-        super().__init__(attack=800, defense=3000)
-
-class Frigate(BattleshipKiller, Battleship):
-    def __init__(self):
-        super().__init__(attack=500, defense=2500)
