c832e428923b799853fd328ac54c0f4978522606
LeYapson <theau.yapi@ynov.com>
2023-10-12 16:04:48 +0200

    walla on fini

--- base_spaceships.py
+++ base_spaceships.py
@@ -4,3 +4,20 @@ class Spaceship:
     def __init__(self, attack=0, defense=0):
         self.attack = attack
         self.defense = defense
+
+    def take_damages(self, damage):
+        """
+        Apply damage to the spaceship's defense.
+
+        :param damage: The amount of damage to apply.
+        :type damage: int
+
+        :raises ValueError: If damage is negative.
+        """
+        if damage < 0:
+            raise ValueError("Damage cannot be negative.")
+
+        self.defense -= damage
+        if self.defense <= 0:
+            self.defense = 0
+            self.is_alive = False
