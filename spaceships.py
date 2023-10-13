4bd6bec317fe8ed70874b4c2923c71365e211a29
LeYapson <theau.yapi@ynov.com>
2023-10-13 10:12:21 +0200

    walla on fini

--- spaceships.py
+++ spaceships.py
@@ -1,52 +1,45 @@
-# In spaceships.py
-
 from base_spaceships import Fighter, Battleship
 
-
-class BattleshipKiller:
-    def __init__(self, attack, defense):
-        self.attack = attack
-        self.defense = defense
-
-    def fire_on(self, target):
-        if isinstance(target, Battleship):
-            target.take_damages(2 * self.attack)
-        else:
-            target.take_damages(self.attack)
-
-
-class FighterKiller:
-    def __init__(self, attack, defense):
-        self.attack = attack
-        self.defense = defense
-
-    def fire_on(self, target):
-        if isinstance(target, Fighter):
-            target.take_damages(2 * self.attack)
-        else:
-            target.take_damages(self.attack)
-
-
-class Interceptor(FighterKiller, Fighter):
+class Interceptor(Fighter):
     def __init__(self):
         super().__init__(attack=180, defense=1000)
 
+    def fire_on(self, target):
+        if isinstance(target, Fighter):
+            target.defense -= self.attack * 2
+        else:
+            target.defense -= self.attack
 
-class Bomber(FighterKiller, Fighter):
+class Bomber(Fighter):
     def __init__(self):
         super().__init__(attack=150, defense=2000)
 
+    def fire_on(self, target):
+        if isinstance(target, Battleship):
+            target.defense -= self.attack * 2
+        else:
+            target.defense -= self.attack
 
-class Cruiser(BattleshipKiller, Battleship):
+class Cruiser(Battleship):
     def __init__(self):
         super().__init__(attack=800, defense=3000)
 
-
-class Frigate(BattleshipKiller, Battleship):
+class Frigate(Battleship):
     def __init__(self):
         super().__init__(attack=500, defense=2500)
 
+    def fire_on(self, target):
+        if isinstance(target, Fighter):
+            target.defense -= self.attack * 2
+        else:
+            target.defense -= self.attack
 
-class Destroyer(BattleshipKiller, Battleship):
+class Destroyer(Battleship):
     def __init__(self):
         super().__init__(attack=650, defense=5000)
+
+    def fire_on(self, target):
+        if isinstance(target, Battleship):
+            target.defense -= self.attack * 2
+        else:
+            target.defense -= self.attack
