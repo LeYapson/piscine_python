ae6e748436656bb7c2038e7802ebe94c3b0b5f3a
LeYapson <theau.yapi@ynov.com>
2023-10-13 09:52:53 +0200

    walla on fini

--- spaceships.py
+++ spaceships.py
@@ -2,46 +2,45 @@
 
 from base_spaceships import Fighter, Battleship
 
-class Interceptor(Fighter):
+
+class BattleshipKiller:
+    def fire_on(self, target):
+        if isinstance(target, Battleship):
+            target.take_damages(2 * self.attack)
+        else:
+            target.take_damages(self.attack)
+
+
+class FighterKiller:
+    def fire_on(self, target):
+        if isinstance(target, Fighter):
+            target.take_damages(2 * self.attack)
+        else:
+            target.take_damages(self.attack)
+
+
+class Interceptor(FighterKiller, Fighter):
     def __init__(self):
         super().__init__(attack=180, defense=1000)
 
-    def fire_on(self, target):
-        if isinstance(target, Fighter):
-            target.take_damages(2 * self.attack)
-        else:
-            target.take_damages(self.attack)
 
-class Bomber(Fighter):
+class Bomber(FighterKiller, Fighter):
     def __init__(self):
         super().__init__(attack=150, defense=2000)
 
-    def fire_on(self, target):
-        if isinstance(target, Battleship):
-            target.take_damages(2 * self.attack)
-        else:
-            target.take_damages(self.attack)
 
-class Cruiser(Battleship):
+class Cruiser(BattleshipKiller, Battleship):
     def __init__(self):
         super().__init__(attack=800, defense=3000)
 
-class Frigate(Battleship):
+
+class Frigate(BattleshipKiller, Battleship):
     def __init__(self):
         super().__init__(attack=500, defense=2500)
 
-    def fire_on(self, target):
-        if isinstance(target, Fighter):
-            target.take_damages(2 * self.attack)
-        else:
-            target.take_damages(self.attack)
 
-class Destroyer(Battleship):
+class Destroyer(BattleshipKiller, Battleship):
     def __init__(self):
         super().__init__(attack=650, defense=5000)
 
-    def fire_on(self, target):
-        if isinstance(target, Battleship):
-            target.take_damages(2 * self.attack)
-        else:
-            target.take_damages(self.attack)
+
