ff484ee9ce3df5ef110d8adae8605d206a1625bd
LeYapson <theau.yapi@ynov.com>
2023-10-23 14:51:00 +0200

    multiple inheritance zebiiii c'est trop durrrrr

--- spaceships.py
+++ spaceships.py
@@ -2,9 +2,6 @@ from base_spaceships import Fighter, Battleship
 
 
 class BattleshipKiller:
-    def __init__(self, attack=100, defense=100):
-        self.attack = attack
-        self.defense = defense
 
     def fire_on(self, target):
         if target is None:
@@ -17,9 +14,6 @@ class BattleshipKiller:
 
 
 class FighterKiller:
-    def __init__(self, attack=100, defense=100):
-        self.attack = attack
-        self.defense = defense
 
     def fire_on(self, target):
         if target is None:
