a69315b59dc78160813339a68efd7d1b96f20ff6
LeYapson <theau.yapi@ynov.com>
2023-10-23 16:52:57 +0200

    est ce qu abdel est trop fort ?

--- spaceships.py
+++ spaceships.py
@@ -2,8 +2,8 @@ from base_spaceships import Fighter, Battleship
 
 
 class BattleshipKiller(Battleship):
-    def __init__(self, attack, defense):
-        super().__init__(attack, defense)
+    def __init__(self):
+        super().__init__(attack=150, defense=2000)
 
     def fire_on(self, target):
         if target is None:
@@ -16,8 +16,8 @@ class BattleshipKiller(Battleship):
 
 
 class FighterKiller(Fighter):
-    def __init__(self, attack, defense):
-        super().__init__(attack, defense)
+    def __init__(self):
+        super().__init__(attack=150, defense=2000)
 
     def fire_on(self, target):
         if target is None:
@@ -31,29 +31,24 @@ class FighterKiller(Fighter):
 
 class Interceptor(FighterKiller, Fighter):
     def __init__(self):
-        super().__init__(attack=180, defense=1000)
-        self.attack = 180
+        super().__init__()
 
 
 class Bomber(BattleshipKiller, Fighter):
     def __init__(self):
-        super().__init__(attack=150, defense=2000)
-        self.attack = 150
+        super().__init__()
 
 
 class Cruiser(Battleship):
     def __init__(self):
-        super().__init__(attack=800, defense=3000)
-        self.attack = 800
+        super().__init__()
 
 
 class Frigate(FighterKiller, Battleship):
     def __init__(self):
-        super().__init__(attack=500, defense=2500)
-        self.attack = 500
+        super().__init__()
 
 
 class Destroyer(BattleshipKiller, Battleship):
     def __init__(self):
-        super().__init__(attack=650, defense=5000)
-        self.attack = 650
+        super().__init__()
