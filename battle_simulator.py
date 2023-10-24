dd068b35a3409d839e07d7dc151f6dc3a77f7c75
LeYapson <theau.yapi@ynov.com>
2023-10-24 15:30:30 +0200

    j'suis meilleur que abdel mdr il a eu 12 erreur quand j'en avait que 4

--- battle_simulator.py
+++ battle_simulator.py
@@ -1,9 +1,20 @@
+import random
+
 from base_spaceships import Spaceship
 
-class Simulator:
-  @staticmethod
-  def _duel_fight(attacker_ship: Spaceship, defender_ship: Spaceship):
-    attacker_ship.fire_on(defender_ship);
 
-    if defender_ship.is_alive == True:
-      defender_ship.fire_on(attacker_ship)
+class Simulator:
+    @staticmethod
+    def _duel_fight(attacker_ship: Spaceship, defender_ship: Spaceship):
+        attacker_ship.fire_on(defender_ship);
+
+        if defender_ship.is_alive == True:
+            defender_ship.fire_on(attacker_ship)
+
+    def _simulate_fight(self, attackers, defenders):
+        for attacker_ship in attackers:
+            if attacker_ship.is_alive:
+                alive_defenders = [ship for ship in defenders if ship.is_alive]
+                if alive_defenders:
+                    defender_ship = random.choice(alive_defenders)
+                    self._duel_fight(attacker_ship, defender_ship)
